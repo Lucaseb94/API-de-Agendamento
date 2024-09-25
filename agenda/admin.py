2c0b8f12517fa5bd793b0626929f8cbb4e034d08
Lucas Miranda <lmirandaeb@gmail.com>
2024-09-25 20:49:51 -0300

    adiciona views comlist

--- agenda/admin.py
+++ agenda/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Agendamento
 
-# Register your models here.
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('nomeCliente', 'dataHorario', 'emailCliente', 'telefoneCliente')
