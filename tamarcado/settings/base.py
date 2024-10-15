9ae339310df0547b3e19e2fa51672efde1af85e8
Lucas Miranda <lmirandaeb@gmail.com>
2024-10-15 06:33:23 -0300

    Adicionando api-de-agendamento-3.onrender.com ao ALLOWED_HOSTS

--- tamarcado/settings/base.py
+++ tamarcado/settings/base.py
@@ -31,7 +31,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED')
+
+ALLOWED_HOSTS = os.environ.get('ALLOWED', '').split(',')
 
 
 # Application definition
