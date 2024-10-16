b38113dfeeafcfe68d037306da03e877d883cf33
Lucas Miranda <lmirandaeb@gmail.com>
2024-10-16 06:26:20 -0300

    configurando deploy db

--- tamarcado/settings/base.py
+++ tamarcado/settings/base.py
@@ -90,7 +90,7 @@ DATABASES = {
         'USER': os.environ.get('DATABASE_USER'), 
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'), 
         'HOST': os.environ.get('DATABASE_HOST'),  
-        'PORT': '',  # Se não precisar de uma porta específica, deixe vazio
+        'PORT': '1433',  # Se não precisar de uma porta específica, deixe vazio
         'OPTIONS': {
             'driver': os.environ.get('DATABASE_DRIVER'),  # Corrigido
         },
