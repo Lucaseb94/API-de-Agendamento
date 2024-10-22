8c6ee14df4af4874653320831b5d7755d5cc90e6
Lucas Miranda <lmirandaeb@gmail.com>
2024-10-22 06:29:47 -0300

    configurando deploy db

--- tamarcado/settings/dev.py
+++ tamarcado/settings/dev.py
@@ -13,11 +13,10 @@ DATABASES = {
         'NAME': os.environ.get('DATABASE_NAME'),  
         'USER': os.environ.get('DATABASE_USER'), 
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'), 
-        'HOST': os.environ.get('DATABASE_HOST'),  
-        'PORT': '1433',  # Se não precisar de uma porta específica, deixe vazio
+        'HOST': 'DESKTOP-9PIP9L1\SQLEXPRESS',
+        'PORT': '',  # Se não precisar de uma porta específica, deixe vazio
         'OPTIONS': {
-            'driver': os.environ.get('DATABASE_DRIVER'),  
+            'driver': 'ODBC Driver 17 for SQL Server',  # Corrigido
         },
     }
 }
-
